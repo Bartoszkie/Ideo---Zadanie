02cfc0220bfe82b1d4deef5b19bcfcd88262ca91
Bartoszkie <bartosz.slysz23@gmail.com>
2019-04-29 17:37:11 +0200

    beta 2.0 commit

---
+++ JS/scrpit.js
@@ -0,0 +1,28 @@
+function searchInput(){
+    const searchVisibility = document.getElementById("search");
+    if(searchVisibility.style.visibility === "hidden"){
+        searchVisibility.style.visibility = "visible";
+    }else {
+        searchVisibility.style.visibility = "hidden";
+    }
+}
+
+function closeOpenNav(){
+    const leftNavbar = document.getElementById("sideNav").offsetLeft;
+    if(leftNavbar == 0){
+        closeNav();
+    }else if(leftNavbar == -150){
+        openNav();
+    }
+}
+
+function closeNav(){
+    const arrow = document.getElementById("arrow").style.transform = "rotate(180deg)";
+    document.getElementById("sideNav").style.left="-150px";
+}
+
+function openNav(){
+    const arrow = document.getElementById("arrow").style.transform = "rotate(360deg)";
+    document.getElementById("sideNav").style.left="0px";
+}
+
